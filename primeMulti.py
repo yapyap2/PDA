307dd28b67774bdbe600075ecd62502a40b6dc2d
yapyap2 <wonwoo42@gmail.com>
2024-03-29 12:41:46 +0900

    test

--- primeMulti.py
+++ primeMulti.py
@@ -21,7 +21,7 @@ primeList = []
 # 병렬 처리를 위한 스레드 풀 생성
 with ThreadPoolExecutor() as executor:
     # 2부터 9999까지 소수 판별 작업을 병렬로 실행
-    results = executor.map(process_number, range(2, 10000))
+    results = executor.map(process_number, range(2, 1000000))
     for result in results:
         if result is not None:
             primeList.append(result)
@@ -32,17 +32,16 @@ print(f"알고리즘 실행 시간: {executionTime} 초")
 
 ioStart = time.time()
 
-# 소수 리스트를 파일에 저장
 for p in primeList:
     file_path = f"./primes/{p}.txt"
     with open(file_path, "w") as file:
         file.write(str(p))
 
-ioEnd = time.time()
-print(f"IO 시간: {ioEnd - ioStart} 초")
 
-# 생성된 파일 삭제
 for filename in os.listdir("./primes"):
     file_path = os.path.join("./primes", filename)
     if os.path.isfile(file_path):
         os.remove(file_path)
+
+ioEnd = time.time()
+print(f"IO 시간: {ioEnd - ioStart} 초")
